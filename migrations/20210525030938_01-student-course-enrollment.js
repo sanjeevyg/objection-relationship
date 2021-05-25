35a13c5ae7107794ab31a7b0b4730eb654e42ef8
sanjeevyg <yogi.sjv@gmail.com>
2021-05-24 22:10:38 -0500

    migration updated

---
+++ migrations/20210525030938_01-student-course-enrollment.js
@@ -0,0 +1,26 @@
+exports.up = async knex => {
+    await knex.schema.createTable("student", table => {
+        table.increments("id")
+        table.string("name")
+    })
+    
+    await knex.schema.createTable("course", table => {
+        table.increments("id")
+        table.string("title")
+    })
+  
+    await knex.schema.createTable("enrollment", table => {
+        table.integer("student_id").references("id").inTable("student")
+        table.integer("course_id").references("id").inTable("course")
+    })
+  
+  };
+  
+  
+  exports.down = async knex => {
+      await knex.schema.dropTableIfExists("enrollment")
+      await knex.schema.dropTableIfExists("course")
+      await knex.schema.dropTableIfExists("student")
+    
+  };
+  
